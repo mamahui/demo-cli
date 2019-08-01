3db56b0fa5a1693e0d895d86fd82e270080e9540
mahui <mahui@g7.com.cn>
2019-08-01 11:09:31 +0800

    feat：完善内容，增加文档编写，发布新版本

---
+++ bin/create-template.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer'); //交互式输入
+const download = require('download-git-repo');// 从git拉取模板
+const argv = require('yargs').argv; //
+const ora = require('ora');
+const fs = require('fs');
+const chalk = require('chalk');
+const spinner = ora('downloading template ...');
+
+const questions = [
+    {
+        type: 'input', name: 'projectName', message: 'please input project name:', validate (val) {
+            if (!val) {
+                return '请输入文件名';
+            }
+            if (fs.existsSync(val)) {
+                return '文件已存在';
+            }
+            else {
+                return true;
+            }
+            ;
+        }
+    }, {
+        type: 'input', name: 'version', message: 'please input project version:', default: '1.0.0', validate (val) {
+            return true;
+        }
+    }, {
+        type: 'input', name: 'description', message: 'please input project description:', default: '', validate (val) {
+            return true;
+        }
+    }, {
+        type: 'input',
+        name: 'repository',
+        message: 'please input address which you will git pull:',
+        default: 'https://github.com:mamahui/vue-template#master'
+    }
+];
+
+const editFile = function({ version, projectName, description }) {
+    fs.readFile(`${process.cwd()}/${projectName}/package.json`, (err, data) => {
+        if (err) throw err;
+        let _data = JSON.parse(data.toString());
+        _data.name = projectName;
+        _data.version = version;
+        _data.description = description;
+        let str = JSON.stringify(_data, null, 4);
+        fs.writeFile(`${process.cwd()}/${projectName}/package.json`, str, function(err) {
+            if (err) throw err;
+        });
+        spinner.succeed();
+    });
+};
+
+/**
+ * @description: 下载模板
+ * @param {type}
+ * @return:
+ */
+const downloadTemplate = function({ repository, version, description, projectName }) {
+    download(repository, projectName, { clone: true }, function(err) {
+        if (err !== 'Error') {
+            editFile({ version, projectName, description });
+            console.log(chalk.green('cli 构建加载完成，可以开始您的项目。'));
+        }
+        else {
+            spinner.succeed();
+            console.log(chalk.red('cli 构建加载错误,请重试。'));
+        }
+        
+        console.log();
+        console.log(chalk.green(`开始项目:  cd ${projectName} && npm install`));
+    });
+};
+inquirer
+.prompt(questions)
+.then(answers => {
+    const version = answers.version;
+    const projectName = answers.projectName;
+    const description = answers.description;
+    const repository = answers.repository;
+    
+    spinner.start();
+    spinner.color = 'green';
+    downloadTemplate({ repository, version, description,projectName });
+});
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# mhh-cli
+通过交互式命令从git拉取模板生成项目
+
+## 使用姿势
+- 全局安装 npm i mhh-cli -g
+- 在项目目录下运行@mhh
+- 根据提示输入项目名称，版本号以及项目描述即可
+
+## 参考文档
+
+- [download-git-repo包从远程(GitHub, GitLab, Bitbucket)拉取文件到本地](https://www.npmjs.com/package/download-git-repo)
+- [Ora 在控制台显示当前加载状态的](https://github.com/sindresorhus/ora)
+- [chalk 控制控制台输出文字的颜色](https://github.com/chalk/chalk)
+- [NodeJs交互式命令行工具inquirer](https://www.npmjs.com/package/inquirer)
